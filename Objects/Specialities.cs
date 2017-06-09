03842eae1b7d349eff290c1873191fd2ca7b5339
Anabel Ramirez <anabel@apparentinc.com>
2017-06-09 12:43:42 -0700

    add passing Specialty.cs.

---
+++ Objects/Specialities.cs
@@ -0,0 +1,205 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using HairSalon;
+
+namespace HairSalon.Objects
+{
+  public class Specialty
+  {
+    private int _id;
+    private string _name;
+
+    public Specialty(string name, int id = 0)
+    {
+      _name = name;
+      _id = id;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public void SetId(int id)
+    {
+      _id = id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string name)
+    {
+      _name = name;
+    }
+
+    public override bool Equals(System.Object otherSpecialty)
+    {
+      if (!(otherSpecialty is Specialty))
+      {
+        return false;
+      }
+      else
+      {
+        Specialty newSpecialty = (Specialty) otherSpecialty;
+        bool idEquality = this.GetId() == newSpecialty.GetId();
+        bool nameEquality = this.GetName() == newSpecialty.GetName();
+        return (idEquality && nameEquality);
+      }
+    }
+    public static List<Specialty> GetAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT * FROM specialty;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Specialty> allSpecialtys = new List<Specialty>{};
+      while (rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        Specialty newSpecialty = new Specialty(name, id);
+        allSpecialtys.Add(newSpecialty);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allSpecialtys;
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM specialty;", conn);
+
+      cmd.ExecuteNonQuery();
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("INSERT INTO specialty (name) OUTPUT INSERTED.id VALUES (@SpecialtyName);", conn);
+      SqlParameter nameParam = new SqlParameter("@SpecialtyName", this.GetName());
+      cmd.Parameters.Add(nameParam);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static Specialty Find(int idToFind)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM specialty WHERE id = @SpecialtyId;", conn);
+      SqlParameter idParam = new SqlParameter("@SpecialtyId", idToFind);
+      cmd.Parameters.Add(idParam);
+      SqlDataReader rdr = cmd.ExecuteReader();
+      int id =  0;
+      string name = null;
+
+      while(rdr.Read())
+      {
+        id = rdr.GetInt32(0);
+        name = rdr.GetString(1);
+      }
+      Specialty foundSpecialty = new Specialty(name, id);
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+      return foundSpecialty;
+    }
+    public static List<Specialty> SearchByName(string nameToSearch)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT * FROM specialty WHERE name = @SearchName;", conn);
+      SqlParameter searchParam = new SqlParameter("@SearchName", nameToSearch);
+      cmd.Parameters.Add(searchParam);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Specialty> matches = new List<Specialty>{};
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        Specialty newSpecialty = new Specialty(name, id);
+        matches.Add(newSpecialty);
+      }
+
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+      return matches;
+    }
+    public void DeleteSingleSpecialty()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM specialty WHERE id = @SpecialtyId;", conn);
+      SqlParameter idParam = new SqlParameter("@SpecialtyId", this.GetId());
+      cmd.Parameters.Add(idParam);
+      cmd.ExecuteNonQuery();
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public void Update(string newName)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("UPDATE specialty SET name = @SpecialtyName OUTPUT INSERTED.name WHERE id = @SpecialtyId;", conn);
+      SqlParameter nameParam = new SqlParameter("@SpecialtyName", newName);
+      SqlParameter idParam = new SqlParameter("@SpecialtyId", this.GetId());
+      cmd.Parameters.Add(nameParam);
+      cmd.Parameters.Add(idParam);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._name = rdr.GetString(0);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+  }
+}
---
+++ Tests/SpecialtyTest.cs
@@ -0,0 +1,114 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using HairSalon.Objects;
+
+namespace HairSalon
+{
+  [Collection("HairSalon")]
+
+  public class SpecialtyTest : IDisposable
+  {
+    public SpecialtyTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void TestSpecialty_DatabaseEmptyAtFirst()
+    {
+      //Arrange
+      List<Specialty> allSpecialtys = new List<Specialty>{};
+
+      //Act
+      List<Specialty> testList = Specialty.GetAll();
+
+      //Assert
+      Assert.Equal(allSpecialtys, testList);
+    }
+    public void Dispose()
+    {
+      Specialty.DeleteAll();
+    }
+
+    [Fact]
+    public void TestSpecialty_Equal_ReturnEqualValues()
+    {
+      //Arrange
+      Specialty newSpecialty = new Specialty("color");
+      Specialty testSpecialty = new Specialty("color");
+
+      //Act, Assert
+      Assert.Equal(newSpecialty, testSpecialty);
+    }
+
+    [Fact]
+    public void TestSpecialty_Save_SavesSpecialtyToDatabase()
+    {
+      //arrange
+      Specialty newSpecialty = new Specialty("color");
+      newSpecialty.Save();
+
+      //Act
+      Specialty savedSpecialty = Specialty.GetAll()[0];
+
+      //assert
+      Assert.Equal(newSpecialty, savedSpecialty);
+    }
+
+    [Fact]
+    public void TestSpecialty_Find_FindsSpecialtyInDatabase()
+    {
+      //arrange
+      Specialty newSpecialty = new Specialty("color");
+      newSpecialty.Save();
+
+      //Act
+      Specialty foundSpecialty = Specialty.Find(newSpecialty.GetId());
+
+      //assert
+      Assert.Equal(newSpecialty, foundSpecialty);
+    }
+    [Fact]
+    public void TestSpecialty_SearchByName_ReturnsMatches()
+    {
+      Specialty specialty1 = new Specialty("color");
+      specialty1.Save();
+      Specialty specialty2 = new Specialty("mens cut");
+      specialty2.Save();
+      Specialty specialty3 = new Specialty("color");
+      specialty3.Save();
+
+      List<Specialty> testList = new List<Specialty>{specialty1, specialty3};
+      List<Specialty> testmatches = Specialty.SearchByName("color");
+
+      Assert.Equal(testList, testmatches);
+    }
+    [Fact]
+    public void TestSpecialtyDelete_DeletesSingleSpecialty()
+    {
+      Specialty newSpecialty1 = new Specialty("color");
+      newSpecialty1.Save();
+      Specialty newSpecialty2 = new Specialty("Anna");
+      newSpecialty2.Save();
+
+      newSpecialty1.DeleteSingleSpecialty();
+
+      List<Specialty> sampleList = new List<Specialty>{newSpecialty2};
+      List<Specialty> testList = Specialty.GetAll();
+
+      Assert.Equal(sampleList, testList);
+    }
+    [Fact]
+    public void TestSpecialty_Update_UpdatesSpecialtyName()
+    {
+      Specialty testSpecialty = new Specialty("Anthony");
+      testSpecialty.Save();
+
+      testSpecialty.Update("color");
+
+      Assert.Equal("color", testSpecialty.GetName());
+    }
+  }
+}
